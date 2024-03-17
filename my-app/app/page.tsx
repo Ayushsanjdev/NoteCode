dc1713e898952f804563f889d7c7fc1deb744bac
ayushsanjdev <ayushsanjpro@gmail.com>
2024-03-17 21:40:57 +0530

    update folder

--- my-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "@/components/common/header/Header";
-
-export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-14 bg-hero-pattern'>
-      <Header />
-    </main>
-  );
-}
